20647b4961f6206c097090850db0a54685a5502f
Tristan <thrawst88@gmail.com>
2018-02-19 14:20:59 +0100

    added map-cap

---
+++ map-cap.js
@@ -0,0 +1,25 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({show: true});
+
+// to prevent JS from doing some float rounding keep as strings
+const alt = '500m'
+const lat = '39.2719844';
+const long = '-111.6372656';
+
+const url = `https://www.google.se/maps/@${lat},${long},${alt}/data=!3m1!1e3?hl=en`;
+
+const satButton = '.widget-settings-button:nth-child(2)';
+
+nightmare
+  .viewport(1100, 700)
+  .goto(url)
+  .wait(4000)
+  .click('.searchbox-hamburger')
+  .wait(satButton)
+  .click(satButton)
+  .wait(1500)
+  .screenshot('./output.png', {x:100, y:100, width:900, height:485})
+  .end()
+  .catch(error => {
+    console.error('Something failed:', error)
+  });
