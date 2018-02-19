bd27d6fb01016ef64e7e38fc9663eae09a033c24
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-02-19 14:49:14 +0100

    changes in map-cap.json

--- map-cap.js
+++ map-cap.js
@@ -2,11 +2,12 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({show: true});
 
 // to prevent JS from doing some float rounding keep as strings
-const alt = '500m'
-const lat = '39.2719844';
-const long = '-111.6372656';
+const alt = '15z'
+const lat = '59.2719844';
+const long = '17.6372656';
 
-const url = `https://www.google.se/maps/@${lat},${long},${alt}/data=!3m1!1e3?hl=en`;
+const url = `https://www.google.se/maps/@${lat},${long},${alt}`;
+//const url = `https://www.google.se/maps/@${lat},${long},${alt}/data=!3m1!1e3?hl=en`;
 
 const satButton = '.widget-settings-button:nth-child(2)';
 
@@ -18,7 +19,7 @@ nightmare
   .wait(satButton)
   .click(satButton)
   .wait(1500)
-  .screenshot('./output.png', {x:100, y:100, width:900, height:485})
+  .screenshot('./output.png', {x:100, y:100, width:900, height:455})
   .end()
   .catch(error => {
     console.error('Something failed:', error)
