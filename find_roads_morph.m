341e009be7884f56088d4eb3c563483636ba8146
Tristan <thrawst88@gmail.com>
2018-02-21 12:30:33 +0100

    full morph alg

--- find_roads_morph.m
+++ find_roads_morph.m
@@ -1,11 +1,75 @@
 function [img] = find_roads_morph(mimg)
-ses = structuring_elems(5);
-for i = 1:numel(ses)
-    se = ses{i};
-    img = imclose(imopen(mimg, se), se);
-end
+strel_size = 5;
+ses = structuring_elems(strel_size);
+
+img = dir_morph(mimg, ses, strel_size, 'erode');
+img = dir_morph(img, ses, strel_size, 'dilate');
+
+img = dir_morph(img, ses, strel_size, 'dilate');
+img = dir_morph(img, ses, strel_size, 'erode');
+
 end
 
+%% directional morph
+function img = dir_morph(img, ses, strel_size, op)
+    offset = (strel_size - 1)/2;
+    % do not modify this
+    tmp_img = padarray(img, [offset, offset], 0, 'both');
+    % we modify *this*
+    out_img = tmp_img;
+    progressbar(sprintf('%s i', op),'j');
+    for i = offset+1:(size(tmp_img,1)-offset)
+        for j = offset+1:(size(tmp_img,2)-offset)
+            % compute sd gray value
+            vals = zeros(numel(ses), 1);
+            for k = 1:numel(ses)
+                se = ses{k};
+                sub_img = tmp_img(i-offset:i+offset,j-offset:j+offset);
+                vals(k) = strel_stdev(sub_img, se);
+            end
+            % morph i,j where sd is minimal
+            [~, idx] = min(vals);
+            sub_img = tmp_img(i-offset:i+offset,j-offset:j+offset);
+            switch op
+                case 'dilate'
+                    out_img(i-offset:i+offset,j-offset:j+offset) = imdilate(sub_img, ses{idx});
+                case 'erode'
+                    out_img(i-offset:i+offset,j-offset:j+offset) = imerode(sub_img, ses{idx});
+                otherwise
+                    warning('Unexpected morph type.');
+            end
+            progressbar([], j/size(tmp_img,2));
+        end
+        progressbar(i/size(tmp_img,1),[]);
+    end
+    progressbar(1);
+    % trim output.
+    img = out_img(offset:end-offset, offset:end-offset);
+    img = contrast_stretch(img);
+end
+
+function stretched = contrast_stretch(img)
+vmin = min(img(:));
+vmax = max(img(:));
+m = 255/(vmax - vmin);  % slope from min to max
+c = 255 - m*vmax;       % find the intercept of the straight line with the axis
+stretched = m*img + c;  % transform the image according to new slope
+end
+
+%% stdev over some se
+function stdev = strel_stdev(im, se)
+    vals = zeros(size(se,1), 1);
+    for i=1:size(im,1)
+        for j=1:size(im,2)
+            if se(i,j) == 1
+                vals(i) = im(i,j);
+            end
+        end
+    end
+    stdev = std(vals);
+end
+
+%% create four directional se's
 function [ses] = structuring_elems(size)
     % structuring elements, these should be 5x5.
     horiz = strel('line', size, 0);
--- morph_test.m
+++ morph_test.m
@@ -1,8 +1,10 @@
 close all;
-%% open 
 img = imread('imgs/02.png');
 g_img = rgb2gray(img);
 
+% this takes some time to run
+sizes = size(g_img);
+g_img = imresize(g_img, 0.25, 'nearest');
 g_img = find_roads_morph(g_img);
 
 %% viz
