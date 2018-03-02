754db28dfb4dbdc3f8c91ee338a0dd3573b3b30f
Tristan <thrawst88@gmail.com>
2018-03-02 21:00:05 +0100

    fixed plain_to_test bug

--- ann/plain_to_test.m
+++ ann/plain_to_test.m
@@ -1,4 +1,15 @@
 % Test image preprocessing
 function out = plain_to_test(img)
-    out = rgb2gray(img) > 250;
+    % We need to mask a very specific shade of yellow
+    red = img(:,:,1); green = img(:,:,2); blue = img(:,:,3);
+    mred = red == 255; 
+    mgreen = green == 242; 
+    mblue = blue == 175;
+    final_mask = mred & mgreen & mblue;
+    red(final_mask) = 255; 
+    green(final_mask) = 255; 
+    blue(final_mask) = 255;
+    img = cat(3, red, green, blue);
+    
+    out = rgb2gray(img) >= 250;
 end
