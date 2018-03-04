d0b85caac59dd295e96ac11f2d20867ee3fb0230
Tristan <thrawst88@gmail.com>
2018-03-04 11:29:55 +0100

    rmse, doesn't work do not attempt

--- ann/rmse.m
+++ ann/rmse.m
@@ -1,12 +1,3 @@
-function r=rmse(e,y,x,fp)
-% Function to calculate root mean square error from a data vector or matrix 
-% and the corresponding estimates.
-% Usage: r=rmse(data,estimate)
-% Note: data and estimates have to be of same size
-% Example: r=rmse(randn(100,100),randn(100,100));
+function perf = rmse(net,varargin)
+perf = mse(net,varargin);
 
-% delete records with NaNs in both datasets first
-I = ~isnan(data) & ~isnan(estimate); 
-data = data(I); estimate = estimate(I);
-
-r=sqrt(sum((data(:)-estimate(:)).^2)/numel(data));
